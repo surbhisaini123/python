4b537c4a5e31890830913688c05af5d6bb5763ce
Surbhi Saini <shurbhisaini2006@gmail.com>
2025-10-28 20:39:24 +0530

    practice question

---
+++ mon27.py
@@ -0,0 +1,32 @@
+# def print_name(Name):
+ 
+#  """
+#  hii i am student 
+#  """
+#  print("hiii    "+str(Name))
+
+
+# print_name('BYYYYY')
+# print(print_name.__doc__)
+
+# def sum_digit(num):
+#     for i in num:
+#         _sum=0
+#         _sum=+num
+#     return _sum
+# sum_digit(1,2,3,4)   
+ # print(s) 
+
+
+# def HCF(a,b):
+#     if a>b:
+#         smaller=b
+#     else:
+#         smaller=a
+#     for i in range(1,smaller+1):
+#         if a%i==0 & b%i==0:
+#             hcf=i
+#             print("hcf is====",hcf)
+# HCF(2,4)            
+            
+# a=int(input("enter first num========"))        
---
+++ tues28.py
@@ -0,0 +1,63 @@
+# num=-9876
+# print(abs(num))
+
+# lst={1,2,3,4,}
+# print(all(lst))
+
+
+# print(divmod(9,2))
+
+# A=range(-10,10)
+# print(list(A))
+
+
+
+# a=[1,2,3,4,5,6,7]
+# def power(a):
+#     return a **2
+# q=list(map(power,a))
+# print(q)
+
+
+
+# producct =1
+
+# lst=(1,2,3,4)
+# for num in 
+
+
+# a=int(input("enter first num========"))
+
+
+def Addition(a,b):
+   return a+b
+def Subtraction(a,b):
+     return a-b
+ 
+def Multiplication(a,b):
+    return a*b
+def Division(a,b):
+    return a/b
+
+
+
+# Addition(a,b)
+# Subtraction(a,b)
+# Multiplication(a,b)
+# Division(a,b)
+
+choice=1,2,3,4
+choice=input(input("enter choice"))
+
+a=float(input("enter first num"))
+b=float(input("enter second num"))
+
+if choice==1:
+    print("Addition",Addition(a,b))
+elif choice==2:   
+    print("substraction",Subtraction(a,b))
+elif choice==3:
+    print("multipllication",Multiplication(a,b))    
+else:
+    print("division",Division(a,b))    
+  
